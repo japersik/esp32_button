e6a5f3c246d859f637c71e04ae1fdd0abaacb0f8
Daniil Marukhlenko <daniil.marukh@gmail.com>
2024-10-24 23:40:40 +0300

    Setup component

---
+++ include/japersik/esp32_button/virt_button.h
@@ -0,0 +1,54 @@
+#ifndef _JAPERSIK_ESP32_BUTTON_VIRT_BUTTON_H
+#define _JAPERSIK_ESP32_BUTTON_VIRT_BUTTON_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef uint8_t ButtonEvent;
+
+// Possible button states
+#define BUTTON_PRESS (1 << 0)         // Button was pressed - event
+#define BUTTON_PRESSING (1 << 1)      // Button is pressed now - state
+#define BUTTON_HOLD (1 << 2)          // Button holding has started - event
+#define BUTTON_HOLDING (1 << 3)       // Button is held down now - state
+#define BUTTON_RELEASE_CLICK (1 << 4) // Button is released - event
+#define BUTTON_RELEASE_HOLD (1 << 5) // Button is released after holding - event
+#define BUTTON_STEP (1 << 6)         // Hold step event - state
+
+typedef void (*EventCallbackFunction)(ButtonEvent);
+typedef void (*MulticlickCallbackFunction)(uint8_t);
+typedef void (*HoldCallbackFunction)(uint32_t hold_duration_ms);
+
+typedef struct {
+  EventCallbackFunction event_callback;
+  MulticlickCallbackFunction multiclick_callback;
+  HoldCallbackFunction hold_callback;
+
+  uint32_t last_event_time_ms;
+  uint32_t last_step_time_ms;
+  uint32_t press_start_time_ms;
+
+  uint32_t bounce_duration_ms;
+  uint32_t hold_duration_ms;
+  uint32_t step_duration_ms;
+  uint32_t multiclick_duration_ms;
+  uint8_t multiclick_counter;
+  ButtonEvent state;
+  bool inverse;
+} VirtButton;
+
+VirtButton *button_new();
+void button_free(VirtButton *);
+void button_set_event_callback(VirtButton *, EventCallbackFunction);
+void button_set_hold_callback(VirtButton *, HoldCallbackFunction);
+void button_set_multiclick_callback(VirtButton *, MulticlickCallbackFunction);
+void button_set_bounce_duration(VirtButton *, uint32_t duration_ms);
+void button_set_step_duration(VirtButton *, uint32_t duration_ms);
+void button_set_bounce_timeut(VirtButton *, uint32_t duration_ms);
+void button_set_multiclick_timeut(VirtButton *self, uint32_t duration_ms);
+void button_set_inverse(VirtButton *, bool inverse);
+
+void button_click_on_time(VirtButton *, bool pressed, uint32_t time_ms);
+ButtonEvent button_get_state(const VirtButton *);
+
+#endif // !_JAPERSIK_ESP32_BUTTON_VIRT_BUTTON_H
---
+++ src/virt_button.c
@@ -0,0 +1,97 @@
+
+#include "japersik/esp32_button/virt_button.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+VirtButton *button_new() {
+  VirtButton *self = (VirtButton *)malloc(sizeof(VirtButton));
+  self->state = 0;
+  self->event_callback = NULL;
+  self->hold_callback = NULL;
+  self->multiclick_callback = NULL;
+  self->last_step_time_ms = 0;
+  self->bounce_duration_ms = 80;
+  self->hold_duration_ms = 1000;
+  self->step_duration_ms = 300;
+  self->multiclick_duration_ms = 400;
+  self->multiclick_counter = 0;
+  return self;
+}
+
+void button_free(VirtButton *self) { free(self); }
+
+void button_set_event_callback(VirtButton *self, EventCallbackFunction f) { self->event_callback = f; }
+void button_set_hold_callback(VirtButton *self, HoldCallbackFunction f) { self->hold_callback = f; }
+void button_set_multiclick_callback(VirtButton *self, MulticlickCallbackFunction f) { self->multiclick_callback = f; }
+void button_set_bounce_duration(VirtButton *self, uint32_t duration_ms) { self->bounce_duration_ms = duration_ms; }
+void button_set_step_duration(VirtButton *self, uint32_t duration_ms) { self->step_duration_ms = duration_ms; }
+void button_set_bounce_timeut(VirtButton *self, uint32_t duration_ms) { self->bounce_duration_ms = duration_ms; }
+void button_set_multiclick_timeut(VirtButton *self, uint32_t duration_ms) {
+  self->multiclick_duration_ms = duration_ms;
+}
+void button_set_inverse(VirtButton *self, bool inverse) { self->inverse = inverse; }
+
+static void process_click_on_time(VirtButton *self, bool pressed, uint32_t event_time_ms) {
+  pressed ^= self->inverse;
+  ButtonEvent state = self->state;
+  state &= ~(BUTTON_PRESS | BUTTON_HOLD | BUTTON_RELEASE_CLICK | BUTTON_RELEASE_HOLD | BUTTON_STEP);
+  // send multiclick callback
+  if (event_time_ms - self->press_start_time_ms >= self->multiclick_duration_ms) {
+    if (self->multiclick_counter > 1) {
+      self->multiclick_callback(self->multiclick_counter);
+    }
+    self->multiclick_counter = 0;
+  }
+  // calculate state
+  if (pressed) {
+    if (!(state & BUTTON_PRESSING)) {
+      self->multiclick_counter++;
+      state |= BUTTON_PRESS | BUTTON_PRESSING;
+      self->press_start_time_ms = event_time_ms;
+    } else {
+      if (state & BUTTON_HOLDING) {
+	if ((event_time_ms - self->last_step_time_ms) <= self->step_duration_ms) {
+	  state |= BUTTON_STEP;
+	  self->last_step_time_ms += self->step_duration_ms;
+	}
+      } else {
+	if ((event_time_ms - self->press_start_time_ms) >= self->hold_duration_ms) {
+	  self->last_step_time_ms = event_time_ms;
+	  state |= BUTTON_HOLD | BUTTON_HOLDING;
+	}
+      }
+    }
+  } else if (state & BUTTON_PRESSING) {
+    if (state & BUTTON_HOLDING) {
+      state |= BUTTON_RELEASE_HOLD;
+    } else {
+      state |= BUTTON_RELEASE_CLICK;
+    }
+    state &= ~(BUTTON_HOLDING | BUTTON_PRESSING);
+  }
+  self->state = state;
+
+  // send event callback
+  if ((state & (BUTTON_PRESS | BUTTON_HOLD | BUTTON_RELEASE_CLICK | BUTTON_RELEASE_HOLD | BUTTON_STEP)) &&
+      self->event_callback != NULL) {
+    self->event_callback(state);
+  }
+  // send hold callback
+  if (state & (BUTTON_RELEASE_HOLD) && self->hold_callback != NULL) {
+    self->hold_callback(event_time_ms - self->press_start_time_ms);
+  }
+}
+
+void button_click_on_time(VirtButton *self, bool pressed, uint32_t event_time_ms) {
+  uint32_t delta_time = event_time_ms - self->last_step_time_ms;
+  if (delta_time < self->bounce_duration_ms) {
+    return;
+  }
+
+  process_click_on_time(self, pressed, event_time_ms);
+}
+
+ButtonEvent button_get_state(const VirtButton *self) { return self->state; }
+
